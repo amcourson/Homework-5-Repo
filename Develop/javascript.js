a993b52bfb5c9082ecd5dd97061da0505031d0d8
amcourson <amcourson09@sbcglobal.net>
2021-01-27 10:46:06 -0600

    adding 10am button and javascript local storage for 10am

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -10,4 +10,12 @@ console.log( $(this).parent().siblings(".col-lg-9").children("textarea")[0].valu
 localStorage.setItem("9am",text)
 })
 var text9am = localStorage.getItem("9am")
-$("#9am").val(text9am)
+$("#9am").val(text9am);
+
+$(".btn-primary").on("click",function(){
+    var text =  $(this).parent().siblings(".col-lg-9").children("textarea")[0].value
+  console.log( $(this).parent().siblings(".col-lg-9").children("textarea")[0].value)
+  localStorage.setItem("10am",text)
+  })
+  var text10am = localStorage.getItem("10am")
+  $("#10am").val(text10am);
