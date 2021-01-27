095a215e3591d8b4d8a489ad1f4a9290632db6aa
amcourson <amcourson09@sbcglobal.net>
2021-01-26 19:54:46 -0600

    adding button clicks and local storage to javascript and html

---
+++ Develop/javascript.js
@@ -0,0 +1,13 @@
+var todaysDate = moment().format('LL');
+
+console.log(todaysDate);
+
+$("#currentDay").text(todaysDate);
+
+$(".btn-primary").on("click",function(){
+  var text =  $(this).parent().siblings(".col-lg-9").children("textarea")[0].value
+console.log( $(this).parent().siblings(".col-lg-9").children("textarea")[0].value)
+localStorage.setItem("9am",text)
+})
+var text9am = localStorage.getItem("9am")
+$("#9am").val(text9am)
